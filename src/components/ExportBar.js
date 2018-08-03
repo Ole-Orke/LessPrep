55dc814b4e376d2735951136135c3266be0b25b1
Monkey <ole.orke@hotmail.com>
2018-08-03 14:16:44 -0700

    add image upload and footer buttons

--- src/components/ExportBar.js
+++ src/components/ExportBar.js
@@ -8,11 +8,21 @@ class ExportBar extends Component {
   render() {
     const exportStyle = {
       background: "linear-gradient(#a0c3ff, #6AA1FF)",
-      height: "6vh"
+      height: "6vh",
+      display: "flex",
+      justifyContent: "space-around"
+    }
+    const exportElementStyle = {
+      padding: "10px",
     }
     return (
       <div style={exportStyle}>
-
+        <div style={exportElementStyle}>
+          <button className="btn btn-primary">Reset</button>
+        </div>
+        <div style={exportElementStyle}>
+          <button className="btn btn-primary">Export</button>
+        </div>
       </div>
     )
   }
