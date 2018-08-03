61262905cbdc94d12eaba813c1bbb09d4a5a9574
khalidwilliams <khalidwill27@gmail.com>
2018-08-03 14:21:17 -0700

    inputs in contentTable

--- src/components/ContentTable.js
+++ src/components/ContentTable.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
 class ContentTable extends Component {
   constructor(props) {
     super(props);
+    this.state={
+      data:[],
+      newData:{concept:'',explanation:''}
+    }
   }
 
   render() {
     const contentStyle = {
       height: "80vh",
       flex: 1,
-      borderLeft: "2px solid #e0e0e0"
+      borderLeft: "2px solid #e0e0e0",
+      width:'100%'
     }
+    const fakeData=[{concept:'Ole',explanation:'Horizonite'}, {concept:'Khalid', explanation:'Dude'}]
     return (
       <div style={contentStyle}>
+        <div className="content-table" style={{border:'solid 2px red', height:'100%', overflow:'scroll'}}>
+        <div className="content-input-container" style={{display:'flex',flexDirection:'row', width:'100%'}}>
+            <input id="ConceptInput" style={{flex:1}}/>
+            <input id="ExplanationInput" stlye={{flex:1}}/>
+        </div>
+        </div>
       </div>
     )
   }
