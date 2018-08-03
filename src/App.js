64c1a6219de132b6c674a890661fc7ef4d454f43
Monkey <ole.orke@hotmail.com>
2018-08-03 11:51:23 -0700

    finish layout

--- src/App.js
+++ src/App.js
@@ -5,16 +5,73 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Window">
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">LessPrep</h1>
+          </header>
+        </div>
+        <div className="workspace-container">
+          <DocumentDisplay />
+          <ContentTable />
+        </div>
+        <ExportBar />
       </div>
     );
+  }
+}
+
+class ContentTable extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const contentStyle = {
+      height: "80vh",
+      flex: 1,
+      borderLeft: "2px solid #e0e0e0"
+    }
+    return (
+      <div style={contentStyle}>
+      </div>
+    )
+  }
+}
+
+class DocumentDisplay extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const documentStyle = {
+      height: "80vh",
+      flex: 1
+    }
+    return (
+      <div style={documentStyle}>
+
+      </div>
+    )
+  }
+}
+
+class ExportBar extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    const exportStyle = {
+      background: "linear-gradient(#a0c3ff, #6AA1FF)",
+      height: "6vh"
+    }
+    return (
+      <div style={exportStyle}>
+
+      </div>
+    )
   }
 }
 
